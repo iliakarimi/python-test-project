bd79aecf0af966c7ebea4cdda8d4e696dd71eff6
ilia.karimi <106839861+iliakarimi@users.noreply.github.com>
2023-05-11 15:47:15 +0330

    Height

---
+++ Height/main.py
@@ -0,0 +1,26 @@
+print('Hello')
+
+print("This is height gauge!!!!")
+gen = input('select male or female:\t(PLEASE type in single word): ')
+while gen == None:
+    print('Try to write')
+    gen = input('select male or female:\t(PLEASE type in single word): ')
+height = int(input('\t Enter your Height: '))
+
+if gen == ('male'):
+    print(f'your Heightmale is: \t {height}')
+    if height >= 90:
+        print('\t go to gym FatAss')
+        
+elif gen == ('female'):
+    if height >= 70:
+        print('\t go to gym Stuped FatAss')
+def friend(name , age=None):
+    print(f'Friend name: {name} \t Friend age: {age}')
+    print("I Don't given my fuck for your Friend Ni**a :>")
+friend(name = str(input('Emter your bestfriend Name: \t')) ,age = int(input('Emter your bestfriend Age: \t')))
+
+massege = 'now get the fuck are here'
+print(ascii(massege))
+
+print('Good lock \t ;)')    
